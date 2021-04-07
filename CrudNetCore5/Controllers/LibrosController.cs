f4947c22269ac33dfd8b5d83824752b019af2573
Edwin202-droid <80547097+Edwin202-droid@users.noreply.github.com>
2021-04-07 14:47:45 -0500

    Agregar archivos de proyecto.

---
+++ CrudNetCore5/Controllers/LibrosController.cs
@@ -0,0 +1,127 @@
+﻿using CrudNetCore5.Data;
+
+using CrudNetCore5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CrudNetCore5.Controllers
+{
+    public class LibrosController : Controller
+    {
+        //Primero importamos el applicationdbcontext porque eso nos permite acceder a la base de datos
+        private readonly ApplicationDbContext _context;
+        //Constructor para inicializar el acceso a la db
+        public LibrosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        public IActionResult Index()
+        {
+            IEnumerable<Libro> listaLibros = _context.Libro;
+            //Lo lleva a la vista de Libros
+            return View(listaLibros);
+        }
+
+        //Http Get Crear Libro
+        public IActionResult Create()
+        {
+            //Retorna un formulario
+            return View();
+        }
+
+        //Http Post Crear Libro
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Libro libro)
+        {
+            //Validando los modelos
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Add(libro);
+                _context.SaveChanges();
+
+                //Un mensaje en patalla que nos indique que el libro fue creado
+                TempData["mensaje"] = "El libro fue creado correctamente";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Http Get Edit: Buscamos el libro y retornamos al view
+        public IActionResult Edit(int? id)
+        {
+            if(id== null|| id == 0)
+            {
+                return NotFound();
+            }
+
+            //Obtener libro
+            var libro = _context.Libro.Find(id);
+
+            if(libro == null)
+            {
+                return NotFound();
+            }
+            return View(libro);
+        }
+
+        //Http Post Actualizar Libro
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Libro libro)
+        {
+            //Validando los modelos
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Update(libro);
+                _context.SaveChanges();
+
+                //Un mensaje en patalla que nos indique que el libro fue actualizado
+                TempData["mensaje"] = "El libro se ha actualizado correctamente";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        //Http Get Delete: Buscamos el libro y retornamos al view
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Obtener libro
+            var libro = _context.Libro.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+            return View(libro);
+        }
+
+        //Http Post Eliminar Libro
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteLibro(int? id)
+        {
+            //Obtener el libro por id
+            var libro = _context.Libro.Find(id);
+
+            if(libro == null)
+            {
+                return NotFound();
+            }
+
+                _context.Libro.Remove(libro);
+                _context.SaveChanges();
+
+                //Un mensaje en patalla que nos indique que el libro fue eliminado
+                TempData["mensaje"] = "El libro se ha eliminado correctamente";
+                return RedirectToAction("Index");
+            
+        }
+    }
+}
---
+++ CrudNetCore5/Models/Libro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+//Tabla Libro en la base de datos 
+
+namespace CrudNetCore5.Models
+{
+    public class Libro
+    {
+        [Key] //Llave primaria de la base de datos, solo con Id se puede hacer la llave primaria
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El titulo es obligatorio")]
+        [StringLength(50, ErrorMessage = "El {0} debe ser al menos {2} y maximo {1} caracteres", MinimumLength = 3)]
+        //Para que el titulo tenga el titulo
+        [Display(Name = "Título")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "La descripcion es obligatoria")]
+        [StringLength(50, ErrorMessage = "El {0} debe ser al menos {2} y maximo {1} caracteres", MinimumLength = 3)]
+        //Para que la descripcion tenga el tilde
+        [Display(Name = "Descripción")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "La fecha es obligatoria")]
+        [DataType(DataType.Date)]
+        //Para que aparezca con las palabras separadas
+        [Display(Name = "Fecha de Lanzamiento")]
+        public DateTime FechaLanzamiento { get; set; }
+
+        [Required(ErrorMessage ="El autor es obligatorio")]
+        public string Autor { get; set; }
+
+        [Required(ErrorMessage ="El precio es obligatorio")]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+        public int Precio { get; set; }
+    }
+}
